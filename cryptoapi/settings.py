0322d54e0defb5e66da4ef2eefbc1a9630204aab
matthias152 <mat.kleska@gmail.com>
2022-03-21 19:44:47 +0100

    upp

--- cryptoapi/settings.py
+++ cryptoapi/settings.py
@@ -121,9 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'crypto/static'),
-)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'crypto/static'),)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
