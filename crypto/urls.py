24972427d3d767e311cfffa96860890dc8e4aae2
matthias152 <mat.kleska@gmail.com>
2022-04-07 14:32:43 +0200

    login, register, fixes

--- crypto/urls.py
+++ crypto/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import *
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', show_crypto_prices),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('buy-crypto', buy_cryptos),
     path('sell-crypto', sell_cryptos),
+    path('register', register_request),
 ]
