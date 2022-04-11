ead811511b0b0385fbc834de471345a8ae845d6b
matthias152 <mat.kleska@gmail.com>
2022-04-11 17:33:44 +0200

    transactions

--- apitest.py
+++ apitest.py
@@ -1,9 +1,11 @@
 #  https://github.com/man-c/pycoingecko
 #  https://www.coingecko.com/pl/api/documentation
 from pycoingecko import CoinGeckoAPI
+from datetime import date, time, datetime
 
 coingecko = CoinGeckoAPI()
 
 bitcoin = coingecko.get_price(ids = 'bitcoin', vs_currencies = 'usd')
+z = datetime.now().time()
 
-print(bitcoin)
+print(z)
--- crypto/admin.py
+++ crypto/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import CryptoWallet, Balance
+from .models import CryptoWallet, Balance, Transaction
 
 # Register your models here.
 
 
 admin.site.register(CryptoWallet)
 admin.site.register(Balance)
+admin.site.register(Transaction)
