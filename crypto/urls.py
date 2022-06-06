b58f66e4d8614c160835f8af72476389d62ac0b2
matthias152 <mat.kleska@gmail.com>
2022-06-06 17:03:37 +0200

    email pdf sender, viewstemplate table, bootstrap start

--- crypto/urls.py
+++ crypto/urls.py
@@ -1,14 +1,16 @@
 from django.urls import path
-from .views import *
+from .views import CustomLoginView, buy_cryptos, sell_cryptos, wallet
+from .views import register_request, transactions_history, pdf_transactions, pdf_transactions_email
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
-    path('', wallet),
+    path('', wallet, name='wallet'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('buy-crypto/', buy_cryptos, name='buycryptos'),
     path('sell-crypto/', sell_cryptos, name='sellcryptos'),
     path('register/', register_request, name='register'),
-    path('transactions/', transactions, name='transactions'),
+    path('transactions/', transactions_history, name='transactions'),
     path('transactions/pdf/', pdf_transactions, name='pdftransactions'),
+    path('transactions/emailpdf', pdf_transactions_email, name='emailatt')
 ]
