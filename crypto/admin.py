242c683166be70bc2b531720e9943c74c6622524
matthias152 <mat.kleska@gmail.com>
2022-06-16 21:20:39 +0200

    update, walletids, sending and receiveing

--- crypto/admin.py
+++ crypto/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import CryptoWallet, Balance, Transaction, BuyPrice
-
-# Register your models here.
+from .models import CryptoWallet, Balance, Transaction, BuyPrice, WalletID
+from .models import DepositWithdraw_Transaction, SendReceive_Transaction
 
 
 admin.site.register(CryptoWallet)
 admin.site.register(Balance)
 admin.site.register(Transaction)
 admin.site.register(BuyPrice)
+admin.site.register(WalletID)
+admin.site.register(DepositWithdraw_Transaction)
+admin.site.register(SendReceive_Transaction)
